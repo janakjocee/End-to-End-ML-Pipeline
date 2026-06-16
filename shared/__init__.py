d6d3d3400bf9002643b35e342dc0bdfb7f5bd644
Janak Raj Joshi <janakjocee@gmail.com>
2026-06-16 10:19:25 +0100

    Add churn business scoring artifacts

--- shared/__init__.py
+++ shared/__init__.py
@@ -8,47 +8,4 @@ Common utilities, models, and exceptions used across all microservices.
 __version__ = "2.0.0"
 __author__ = "ML Platform Team"
 
-from shared.utils.logger import get_logger
-from shared.utils.database import CacheManager, DatabaseManager
-from shared.utils.storage import StorageManager
-from shared.models.schemas import (
-    PredictionRequest,
-    PredictionResponse,
-    BatchPredictionRequest,
-    BatchPredictionResponse,
-    ModelMetadata,
-    FeatureSchema,
-    DriftReport,
-    TrainingConfig,
-    EvaluationMetrics
-)
-from shared.exceptions.custom_exceptions import (
-    MLPlatformException,
-    DataValidationError,
-    ModelNotFoundError,
-    FeatureStoreError,
-    DriftDetectionError,
-    TrainingError
-)
-
-__all__ = [
-    'get_logger',
-    'DatabaseManager',
-    'StorageManager',
-    'CacheManager',
-    'PredictionRequest',
-    'PredictionResponse',
-    'BatchPredictionRequest',
-    'BatchPredictionResponse',
-    'ModelMetadata',
-    'FeatureSchema',
-    'DriftReport',
-    'TrainingConfig',
-    'EvaluationMetrics',
-    'MLPlatformException',
-    'DataValidationError',
-    'ModelNotFoundError',
-    'FeatureStoreError',
-    'DriftDetectionError',
-    'TrainingError'
-]
+__all__ = ["__version__", "__author__"]
